97e713636708640ac7ec4ae3fc27a362019866ef
Oracso <85285983+Oracso@users.noreply.github.com>
2024-01-30 18:02:49 +0000

    Initial Commit

---
+++ QuickMaths/QuickMathsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickMathsApp.swift
+//  QuickMaths
+//
+//  Created by Oscar Hardy on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct QuickMathsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
